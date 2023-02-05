aac34d8def3d696e55409f0e4ca45d74000b1503
João Gabriel <main.joaogabrielsg@gmail.com>
2023-02-05 15:39:14 -0300

    refactor: move mapping to MongoHelper

--- src/infra/db/mongodb/account-repository/account.ts
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -1,4 +1,3 @@
-import { type Document, type WithId } from 'mongodb'
 import { type AddAccountRepository } from '../../../../data/protocols/add-account-repository'
 import { type AccountModel } from '../../../../domain/models/account'
 import { type AddAccountModel } from '../../../../domain/usecases/add-account'
@@ -9,8 +8,8 @@ export class AccountMongoRepository implements AddAccountRepository {
     const accountCollection = MongoHelper.getCollection('accounts')
 
     const { insertedId } = await accountCollection.insertOne(accountData)
-    const { _id, ...accountFind } = await accountCollection.findOne() as WithId<Document>
+    const account = await accountCollection.findOne({ _id: insertedId })
 
-    return Object.assign({}, accountFind, { id: insertedId }) as unknown as AccountModel
+    return MongoHelper.map(account)
   }
 }
