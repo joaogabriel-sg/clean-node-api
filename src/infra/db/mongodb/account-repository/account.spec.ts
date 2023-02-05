f73a1bc0293acd2109558b1875105107c74c7a13
João Gabriel <main.joaogabrielsg@gmail.com>
2023-02-05 15:31:19 -0300

    test: ensure AccountMongoRepository returns an account on success

---
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -0,0 +1,29 @@
+import { MongoHelper } from '../helpers/mongo-helper'
+import { AccountMongoRepository } from './account'
+
+describe('Account Mongo Repository', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL as string)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  test('should return an account on success', async () => {
+    const sut = new AccountMongoRepository()
+    const data = {
+      name: 'any_name',
+      email: 'any_email@mail.com',
+      password: 'any_password'
+    }
+
+    const account = await sut.add(data)
+
+    expect(account).toBeTruthy()
+    expect(account.id).toBeTruthy()
+    expect(account.name).toBe(data.name)
+    expect(account.email).toBe(data.email)
+    expect(account.password).toBe(data.password)
+  })
+})
