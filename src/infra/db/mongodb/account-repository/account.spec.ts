5ba4d87a34c195dc74b9987f1d8e0a3c5ac1d91c
João Gabriel <main.joaogabrielsg@gmail.com>
2023-02-05 15:33:46 -0300

    refactor: clear database between tests

--- src/infra/db/mongodb/account-repository/account.spec.ts
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -14,6 +14,11 @@ describe('Account Mongo Repository', () => {
     await MongoHelper.disconnect()
   })
 
+  beforeEach(async () => {
+    const accountCollection = MongoHelper.getCollection('accounts')
+    await accountCollection.deleteMany({})
+  })
+
   test('should return an account on success', async () => {
     const sut = makeSut()
     const data = {
